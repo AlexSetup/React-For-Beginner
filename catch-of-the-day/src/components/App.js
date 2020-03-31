13980037ed89ee128ad2164482240011c26d023c
Alexandre T <thorel.alexandre@gmail.com>
2020-03-31 22:12:19 +0200

    Authentication

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -33,7 +33,6 @@ class App extends React.Component {
   }
 
   componentDidUpdate() {
-    console.log(this.state.order);
     localStorage.setItem(
       this.props.match.params.storeId,
       JSON.stringify(this.state.order)
@@ -120,6 +119,7 @@ class App extends React.Component {
           deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
+          storeId={this.props.match.params.storeId}
         />
       </div>
     );
